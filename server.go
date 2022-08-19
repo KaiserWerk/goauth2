6ab161231fb8361a5aa2541cb2ce52e6d622ab17
Robin Kaiser <web@r-k.mx>
2022-08-20 00:22:32 +0200

    reworked the server, added constructor

--- server.go
+++ server.go
@@ -1,338 +1,152 @@
 package goauth
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"html/template"
-	"io"
-	"net/http"
-	"net/url"
+	"github.com/KaiserWerk/goauth2/assets"
 	"time"
+
+	"github.com/KaiserWerk/goauth2/storage"
+	"github.com/KaiserWerk/goauth2/token"
+	"github.com/KaiserWerk/goauth2/types"
 )
 
-type TokenResponse struct {
-	AccessToken string
-	Expires     uint64
-	Scopes      []string
-	State       string
-}
+type (
+	// Storage contains the storage implementations required for operations.
+	Storage struct {
+		DeviceCodeRequestStorage storage.DeviceCodeStorage
+		SessionStorage           storage.SessionStorage
+		UserStorage              storage.UserStorage
+		ClientStorage            storage.ClientStorage
+		TokenStorage             storage.TokenStorage
+	}
+	// Templates contains the templates displayed for the user
+	Templates struct {
+		Login             []byte
+		AuthorizationCode []byte
+		ImplicitGrant     []byte
+		DeviceCode        []byte
+	}
+	// Flags contains feature flages to enable/disable particular features.
+	Flags struct {
+		// PKCE is not currently implemented
+		PKCE bool
+		// OIDC (= OpenID Connect) is not currently implemented
+		OIDC bool
+	}
+	// Policies represents constraints and requirements for proper operation.
+	Policies struct {
+		DeviceCodeLength   int
+		UserCodeLength     int
+		AccessTokenLength  int
+		RefreshTokenLength int
+		ClientSecretLength int
+		// IDTokenLength only relates to OpenID Connect
+		IDTokenLength int
 
-// A Server handles all HTTP requests relevant to the OAuth2 authorization processes. A Server must not be modified
-// after first use.
-type Server struct {
-	PublicBaseURL            string
-	DeviceCodeRequestStorage DeviceCodeStorage
-	SessionStorage           SessionStorage
-	UserStorage              UserStorage
-	ClientStorage            ClientStorage
-	TokenStorage             TokenStorage
-	TokenSource              TokenSource
-	EnabledGrantTypes        []GrantType
-	EnablePKCE               bool
+		AccessTokenLifetime  time.Duration
+		RefreshTokenLifetime time.Duration
+	}
+	// A Server handles all HTTP requests relevant to the OAuth2 authorization processes. A Server must not be modified
+	// after first use.
+	Server struct {
+		PublicBaseURL string
+		Storage       Storage
+		Template      Templates
+		Flags         Flags
+		Policies      Policies
+		TokenSource   token.TokenSource
+		GrantTypes    []types.GrantType
+	}
+)
+
+// NewDefaultServer returns a *Server with set default values:
+//
+//  • PublicBaseURL: is set to 'http://localhost' without a port. It is required for redirect-based authorization flows.
+//
+//  • Storage: each store uses a corresponding in-memory implementation, e.g. MemoryClientStorage.
+//
+//  • Templates: the default templates from this library are used. They are not overly pretty, but they get their job done.
+//
+//  • Flags: all flags remain at their default value.
+//
+//  • Policies: sensible lengths and lifetime which ensure a certain degree of security.
+//
+//  • TokenSource: uses a ready-to-use in-memory implementation, namely DefaultTokenSource.
+//
+//  • GrantTypes: all implemented grant types are listed here.
+//
+// You should probably alter the PublicBaseURL and add at least one Client and User.
+func NewDefaultServer() *Server {
+	return &Server{
+		PublicBaseURL: "http://localhost",
+		Storage: Storage{
+			DeviceCodeRequestStorage: storage.NewMemoryDeviceCodeRequestStorage(),
+			SessionStorage:           storage.NewMemorySessionStorage(),
+			UserStorage:              storage.NewMemoryUserStorage(),
+			ClientStorage:            storage.NewMemoryClientStorage(),
+			TokenStorage:             storage.NewMemoryTokenStorage(),
+		},
+		Template: Templates{
+			Login:             assets.LoginPageTemplate,
+			AuthorizationCode: assets.AuthorizationCodeTemplate,
+			ImplicitGrant:     assets.ImplicitFlowTemplate,
+			DeviceCode:        assets.DeviceCodeTemplate,
+		},
+		Flags: Flags{},
+		Policies: Policies{
+			DeviceCodeLength:     70,
+			UserCodeLength:       6,
+			AccessTokenLength:    120,
+			RefreshTokenLength:   80,
+			ClientSecretLength:   75,
+			IDTokenLength:        255,
+			AccessTokenLifetime:  1 * time.Hour,
+			RefreshTokenLifetime: 24 * time.Hour,
+		},
+		TokenSource: token.DefaultTokenSource,
+		GrantTypes:  []types.GrantType{types.DeviceCode, types.AuthorizationCode},
+	}
 }
 
 // HandleAuthorizationCodeRequest previously did something.
 //
 // Deprecated: do not use it now
-func (s *Server) HandleAuthorizationCodeRequest(w http.ResponseWriter, r *http.Request) error {
-	// if the user is not logged in
-	// redirect to login page with redirect back url
-
-	q := r.URL.Query()
-	responseType := q.Get("response_type")
-	grantType := q.Get("authorization_code")
-	clientID := q.Get("client_id")
-	redirectURI := q.Get("redirect_uri")
-	scope := q.Get("scope")
-	state := q.Get("state")
-	fmt.Println("response type:", responseType)
-	fmt.Println("grant type:", grantType)
-	fmt.Println("client id:", clientID)
-	fmt.Println("redirect uri:", redirectURI)
-	fmt.Println("scope:", scope)
-	fmt.Println("state:", state)
-	switch {
-	case responseType == "code":
-		if r.Method == http.MethodGet {
-			// write authorize template
-		} else if r.Method == http.MethodPost {
-			username := r.FormValue("_username")
-			password := r.FormValue("_password")
-			u, err := s.UserStorage.GetByUsername(username)
-			if err != nil {
-				http.Error(w, "failed to find user", http.StatusNotFound)
-				return err
-			}
-
-			if u.Password != password {
-
-			}
-		}
-	}
-
-	http.Error(w, "failed", http.StatusInternalServerError)
-
-	return errors.New("undefined grant type")
-}
-
-func (s *Server) HandleDeviceCodeAuthorizationRequest(w http.ResponseWriter, r *http.Request) error { // Step 1
-	if r.Method != http.MethodPost {
-		http.Error(w, "disallowed method", http.StatusBadRequest)
-		return fmt.Errorf("method %s not allowed", r.Method)
-	}
-
-	if ct := r.Header.Get("Content-Type"); ct != "application/x-www-form-urlencoded" {
-		http.Error(w, "wrong content type header", http.StatusBadRequest)
-		return fmt.Errorf("expected content type header to be 'application/x-www-form-urlencoded'. got '%s'", ct)
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "error reading request body", http.StatusBadRequest)
-		return fmt.Errorf("failed to read request body: %s", err.Error())
-	}
-
-	values, err := url.ParseQuery(string(body))
-	if err != nil {
-		http.Error(w, "malformed request body", http.StatusBadRequest)
-		return fmt.Errorf("failed to parse request body: %s", err.Error())
-	}
-
-	if !values.Has("client_id") {
-		http.Error(w, "missing request parameter", http.StatusBadRequest)
-		return fmt.Errorf("missing request parameter '%s'", "client_id")
-	}
-
-	// TODO: check if valid client
-
-	userCode := generateCode(8)
-	req := DeviceCodeRequest{
-		ClientID: values.Get("client_id"),
-		Response: DeviceCodeResponse{
-			DeviceCode:              generateCode(60),
-			UserCode:                userCode,
-			VerificationURI:         s.PublicBaseURL + "/device",
-			VerificationURIComplete: s.PublicBaseURL + "/device?user_code=" + userCode,
-			ExpiresIn:               1800,
-			Interval:                5,
-		},
-	}
-	if err := s.DeviceCodeRequestStorage.Add(req); err != nil {
-		http.Error(w, "failed to store request", http.StatusInternalServerError)
-		return fmt.Errorf("failed to store request: %s", err.Error())
-	}
-
-	if err := json.NewEncoder(w).Encode(req.Response); err != nil {
-		http.Error(w, "failed to write JSON", http.StatusInternalServerError)
-		return fmt.Errorf("failed to write JSON: %s", err.Error())
-	}
-
-	return nil
-}
-
-func (s *Server) HandleDeviceCodeUserAuthorization(w http.ResponseWriter, r *http.Request, loginURL string) error {
-	redirectURL := r.URL.Path
-	if redirectURL != "" {
-		loginURL += "?redirect_uri=" + redirectURL
-	}
-	_, err := s.isLoggedIn(r)
-	if err != nil {
-		http.Redirect(w, r, loginURL, http.StatusSeeOther)
-		return nil
-	}
-	// user is certainly logged in from here on
-
-	if r.Method == http.MethodGet {
-		// TODO: set data!
-		data := struct {
-			Message         string
-			ApplicationName string
-		}{
-			Message:         "",
-			ApplicationName: "My Cool Test App",
-		}
-		if err := ExecuteTemplate(w, "device_code.gohtml", data); err != nil {
-			http.Error(w, "failed to find template", http.StatusNotFound)
-			return fmt.Errorf("failed to find template: %s", err.Error())
-		}
-		return nil
-	} else if r.Method == http.MethodPost {
-		userCode := r.FormValue("_user_code")
-		if userCode == "" {
-			http.Error(w, "empty user code", http.StatusBadRequest)
-			return fmt.Errorf("user failed to provide a user code")
-		}
-
-		deviceRequest, err := s.DeviceCodeRequestStorage.Get(userCode)
-		if err != nil {
-			http.Error(w, "failed to find device code request", http.StatusNotFound)
-			return fmt.Errorf("failed to find device code request")
-		}
-
-		deviceRequest.TokenResponse = DeviceCodeTokenResponse{
-			AccessToken: generateCode(120),
-			TokenType:   "bearer",
-			ExpiresIn:   86400,
-		}
-
-		if err := s.DeviceCodeRequestStorage.Update(deviceRequest); err != nil {
-			http.Error(w, "failed to update device code request", http.StatusNotFound)
-			return fmt.Errorf("failed to update device code request")
-		}
-
-		http.Error(w, "success. you can close this window.", http.StatusOK)
-		return nil
-	}
-
-	return nil
-}
-
-func (s *Server) HandleDeviceTokenRequest(w http.ResponseWriter, r *http.Request) error {
-	if r.Method != http.MethodPost {
-		http.Error(w, "disallowed method", http.StatusBadRequest)
-		return fmt.Errorf("method %s not allowed", r.Method)
-	}
-
-	if ct := r.Header.Get("Content-Type"); ct != "application/x-www-form-urlencoded" {
-		http.Error(w, "wrong content type header", http.StatusBadRequest)
-		return fmt.Errorf("expected content type header to be 'application/x-www-form-urlencoded'. got '%s'", ct)
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "error reading request body", http.StatusBadRequest)
-		return fmt.Errorf("failed to read request body: %s", err.Error())
-	}
-
-	values, err := url.ParseQuery(string(body))
-	if err != nil {
-		http.Error(w, "malformed request body", http.StatusBadRequest)
-		return fmt.Errorf("failed to parse request body: %s", err.Error())
-	}
-
-	if !values.Has("client_id") || !values.Has("grant_type") || !values.Has("device_code") {
-		http.Error(w, "missing request parameter(s)", http.StatusBadRequest)
-		return fmt.Errorf("missing request parameter(s)")
-	}
-
-	deviceRequest, err := s.DeviceCodeRequestStorage.Find(values.Get("device_code"), values.Get("client_id"))
-	if err != nil {
-		http.Error(w, "failed to find request", http.StatusBadRequest)
-		return fmt.Errorf("failed to find request: %s", err.Error())
-	}
-
-	if deviceRequest.TokenResponse.AccessToken == "" {
-		http.Error(w, `{"error": "authorization_pending"}`, http.StatusOK)
-	} else {
-		if err := json.NewEncoder(w).Encode(deviceRequest.TokenResponse); err != nil {
-			http.Error(w, "failed to serialize JSON", http.StatusBadRequest)
-			return fmt.Errorf("failed to serialize JSON: %s", err.Error())
-		}
-	}
-
-	return nil
-}
-
-func (s *Server) HandleUserLogin(w http.ResponseWriter, r *http.Request) error {
-	_, err := s.isLoggedIn(r)
-	if err == nil {
-		http.Error(w, "You are already logged in! You can perform authorizations now.", http.StatusOK)
-		return nil
-	}
-
-	if r.Method == http.MethodGet {
-		if err := ExecuteTemplate(w, "login.gohtml", nil); err != nil {
-			http.Error(w, "failed to find template", http.StatusNotFound)
-			return fmt.Errorf("failed to find template '%s'", "login.gohtml")
-		}
-		return nil
-	} else if r.Method == http.MethodPost {
-		username := r.FormValue("_username")
-		password := r.FormValue("_password")
-		u, err := s.UserStorage.GetByUsername(username)
-		if err != nil {
-			http.Error(w, "failed to find user", http.StatusNotFound)
-			return err
-		}
-
-		if u.Password != password {
-			http.Error(w, "passwords didn't match", http.StatusNotFound)
-			return fmt.Errorf("passwords didn't match")
-		}
-
-		session := Session{
-			ID:     generateCode(45),
-			UserID: u.ID,
-		}
-		if err := s.SessionStorage.Add(session); err != nil {
-			http.Error(w, "failed to add session", http.StatusNotFound)
-			return fmt.Errorf("failed to add session: %s", err.Error())
-		}
-
-		http.SetCookie(w, &http.Cookie{
-			Name:    "GOAUTH_SID",
-			Value:   session.ID,
-			Expires: time.Now().Add(30 * 24 * time.Hour),
-		})
-
-		if red := r.URL.Query().Get("redirect_uri"); red != "" {
-			http.Redirect(w, r, red, http.StatusSeeOther)
-		}
-
-		return nil
-	}
-
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	return fmt.Errorf("method not allowed")
-}
-
-func (s *Server) isLoggedIn(r *http.Request) (User, error) {
-	sid, err := getSessionIDFromRequest(r)
-	if err != nil || sid == "" {
-		return User{}, nil
-	}
-
-	session, err := s.SessionStorage.Get(sid)
-	if err != nil {
-		return User{}, fmt.Errorf("user had session ID, but was not found")
-	}
-
-	user, err := s.UserStorage.Get(session.UserID)
-	if err != nil {
-		return User{}, fmt.Errorf("valid session, but didn't find user")
-	}
-
-	return user, nil
-}
-
-func getSessionIDFromRequest(r *http.Request) (string, error) {
-	c, err := r.Cookie("GOAUTH_SID")
-	if err != nil {
-		return "", err
-	}
-
-	return c.Value, nil
-}
-
-func generateCode(length int) string {
-	b := make([]byte, length)
-	_, _ = rand.Read(b)
-	return base64.RawStdEncoding.EncodeToString(b)
-}
-
-func ExecuteTemplate(w io.Writer, file string, data interface{}) error {
-	tmplContent, err := GetTemplate(file)
-	if err != nil {
-		return err
-	}
-
-	tmpl := template.Must(template.New(file).Parse(string(tmplContent)))
-	if err = tmpl.Execute(w, data); err != nil {
-		return err
-	}
-
-	return nil
-}
+//func (s *Server) HandleAuthorizationCodeRequest(w http.ResponseWriter, r *http.Request) error {
+//	// if the user is not logged in
+//	// redirect to login page with redirect back url
+//
+//	q := r.URL.Query()
+//	responseType := q.Get("response_type")
+//	grantType := q.Get("authorization_code")
+//	clientID := q.Get("client_id")
+//	redirectURI := q.Get("redirect_uri")
+//	scope := q.Get("scope")
+//	state := q.Get("state")
+//	fmt.Println("response type:", responseType)
+//	fmt.Println("grant type:", grantType)
+//	fmt.Println("client id:", clientID)
+//	fmt.Println("redirect uri:", redirectURI)
+//	fmt.Println("scope:", scope)
+//	fmt.Println("state:", state)
+//	switch {
+//	case responseType == "code":
+//		if r.Method == http.MethodGet {
+//			// write authorize template
+//		} else if r.Method == http.MethodPost {
+//			username := r.FormValue("_username")
+//			password := r.FormValue("_password")
+//			u, err := s.UserStorage.GetByUsername(username)
+//			if err != nil {
+//				http.Error(w, "failed to find user", http.StatusNotFound)
+//				return err
+//			}
+//
+//			if u.Password != password {
+//
+//			}
+//		}
+//	}
+//
+//	http.Error(w, "failed", http.StatusInternalServerError)
+//
+//	return errors.New("undefined grant type")
+//}
