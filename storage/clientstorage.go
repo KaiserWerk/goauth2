47bdc359ac4068690b5e839267ea37cf58e2ecc9
Robin Kaiser <web@r-k.mx>
2022-08-23 23:34:22 +0200

    improved client storage

--- storage/clientstorage.go
+++ storage/clientstorage.go
@@ -1,13 +1,20 @@
 package storage
 
+import "fmt"
+
 // A Client represents an application. A Client's ID must be unique.
 type Client struct {
 	ID              string
 	Secret          string
 	ApplicationName string
+	RedirectURLs    []string
 }
 
 type ClientStorage interface {
 	Get(string) (Client, error)
 	Set(Client) error
+}
+
+func (c *Client) String() string {
+	return fmt.Sprintf("%s (%s)", c.ApplicationName, c.ID)
 }
