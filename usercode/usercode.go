da708cdf2b51eb9ec880982fd127cae8d4fd85a9
Robin Kaiser <web@r-k.mx>
2022-08-23 23:33:45 +0200

    add user code generator

---
+++ usercode/usercode.go
@@ -0,0 +1,31 @@
+package usercode
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"strings"
+)
+
+type Generator interface {
+	Generate() (string, error)
+}
+
+type UCGenerator struct{}
+
+var DefaultUCGenerator = &UCGenerator{}
+
+// Generate generates a user code with length 9 in the form of 'XXXX-XXXX'
+func (ucg *UCGenerator) Generate() (string, error) {
+	b := make([]byte, 9)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	b[4] = byte('-') // yes, a dash
+
+	str := base64.RawStdEncoding.EncodeToString(b)
+	str = strings.ToUpper(str)
+
+	return str, nil
+}
