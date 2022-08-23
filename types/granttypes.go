e51105769b3c950ae4e75a5de616e8d11caad8f4
Robin Kaiser <web@r-k.mx>
2022-08-23 23:36:25 +0200

    improve grant types

--- types/granttypes.go
+++ types/granttypes.go
@@ -5,6 +5,24 @@ type GrantType uint8
 const (
 	AuthorizationCode GrantType = iota
 	DeviceCode
+	Implicit
 	ClientCredentials
 	ResourceOwnerPasswordCredentials
 )
+
+func (g GrantType) String() string {
+	switch g {
+	case AuthorizationCode:
+		return "authorization_code"
+	case DeviceCode:
+		return "device_code"
+	case Implicit:
+		return "implicit"
+	case ClientCredentials:
+		return "client_credentials"
+	case ResourceOwnerPasswordCredentials:
+		return "resource_owner_password_credentials"
+	}
+
+	return "invalid grant type"
+}
