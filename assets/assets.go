32f7f4389597f48c7a8bd4b0906fb64014e5c81e
Robin Kaiser <web@r-k.mx>
2022-08-27 20:28:51 +0200

    slightly reworked the templates

--- assets/assets.go
+++ assets/assets.go
@@ -4,14 +4,19 @@ import (
 	_ "embed"
 )
 
+// The LoginPageTemplate is the HTML template used for showing the login form to the user.
 //go:embed templates/login.gohtml
 var LoginPageTemplate []byte
 
+// The DeviceCodeTemplate is the HTML template used for the Device Code Grant.
 //go:embed templates/device_code.gohtml
 var DeviceCodeTemplate []byte
 
+// The ImplicitGrantTemplate is the HTML template used for the Implicit Grant.
 //go:embed templates/implicit_flow.gohtml
-var ImplicitFlowTemplate []byte
+var ImplicitGrantTemplate []byte
 
+// The AuthorizationCodeTemplate is the HTML template used for the Authorization Code Grant and will, by default,
+// also be used for PKCE and OpenID Connect.
 //go:embed templates/authorization_code.gohtml
 var AuthorizationCodeTemplate []byte
